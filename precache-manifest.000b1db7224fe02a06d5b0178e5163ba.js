17658e8da316a96fd937562f546f23e1223742a9
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-02-06 12:21:19 +0300

    Updates

---
+++ precache-manifest.000b1db7224fe02a06d5b0178e5163ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b809e5a2c841c976e75e9ca8b63ce5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fa93600e14d0d1048c9",
+    "url": "/static/css/main.c7ab3b04.chunk.css"
+  },
+  {
+    "revision": "63e53dfe377321318dec",
+    "url": "/static/js/2.22d864fb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.22d864fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa93600e14d0d1048c9",
+    "url": "/static/js/main.357e6878.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
